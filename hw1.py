7915c563e31d3967a0707320a907af15498eaa71
Shcsirf <Shcsirf@gmail.com>
2014-02-02 18:00:56 -0500

    finished python version of program

--- hw1.py
+++ hw1.py
@@ -6,6 +6,18 @@
 # Class: Cse 305
 # ===========================================================================
 
+# ***************************************************************************
+# |hw1|
+#
+# -The purose of this method is to read the contents of a file and print to 
+# a designated file each character of the file with a < before the character
+# and a > after. If it's a newline character it prints a < followed by a >
+# on the next line.
+#
+# Variables Passed in:
+#                     -Name of input file
+#                     -Name of output file
+# ***************************************************************************
 def hw1(inputFile, outputFile):
 	contents = ''
 	with open(inputFile) as f:
@@ -16,12 +28,9 @@ def hw1(inputFile, outputFile):
 	ret = open(outputFile, 'w+')
 	while length_contents > 0:
 		if contents[pos] == '\n':
-			ret.write('<' + '\n' + '>\n')
+			ret.write('>' + '\n' + '<\n')
 		else:
 			ret.write('<'+contents[pos]+'>\n')
 		pos = pos + 1
 		length_contents = length_contents - 1
 	ret.close()
-
-
-hw1('foo.txt', 'boo.txt')
