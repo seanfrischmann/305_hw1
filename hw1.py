58d1891ed04b6bf7b9dcd7f37c12abc7d052f7d6
Sean Frischmann <shcsirf@gmail.com>
2014-02-05 17:24:36 -0500

    Finished python version

--- hw1.py
+++ hw1.py
@@ -10,8 +10,8 @@
 # |hw1|
 #
 # -The purose of this method is to read the contents of a file and print to 
-# a designated file each character of the file with a < before the character
-# and a > after. If it's a newline character it prints a < followed by a >
+# a designated file each character of the file with a > before the character
+# and a < after. If it's a newline character it prints a > followed by a <
 # on the next line.
 #
 # Variables Passed in:
